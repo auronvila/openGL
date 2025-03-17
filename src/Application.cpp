4ffa2955d7a94c0f5ebfaed0181bc2c75a534853
auronvila <auronvila1121@gmail.com>
2025-03-17 17:29:19 +0300

    fix glDrawElements invalid param call

--- src/Application.cpp
+++ src/Application.cpp
@@ -177,7 +177,7 @@ int main(void) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        GlCall(glDrawElements(GL_TRIANGLES, 6,GL_INT, nullptr));
+        GlCall(glDrawElements(GL_TRIANGLES, 6,GL_UNSIGNED_INT, nullptr));
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
