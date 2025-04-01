336fbd7ff888a77cba1102e20b609208fd146ba4
auronvila <auronvila1121@gmail.com>
2025-04-01 10:11:53 +0300

    using vertex arrays

--- res/shaders/Basic.shader
+++ res/shaders/Basic.shader
@@ -1,17 +1,14 @@
 #shader vertex
-#version 120
-
-attribute vec4 position;
-
+#version 330 core
+layout(location = 0) in vec4 position;
 void main() {
     gl_Position = position;
 }
 
 #shader fragment
-#version 120
-// using uniforms to assign the value from the cpp code
+#version 330 core
 uniform vec4 u_color;
-
+out vec4 color;
 void main() {
-    gl_FragColor = u_color;
+    color = u_color;
 }
--- src/Application.cpp
+++ src/Application.cpp
@@ -79,7 +79,6 @@ static unsigned int CompiledShader(unsigned int type, const std::string &source)
     return id;
 }
 
-
 static unsigned int CreateShader(const std::string &vertexShader, const std::string &fragmentShader) {
     unsigned int program = glCreateProgram();
     unsigned int vs = CompiledShader(GL_VERTEX_SHADER, vertexShader);
@@ -124,6 +123,10 @@ int main(void) {
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -151,10 +154,15 @@ int main(void) {
         0, 1, 2,
         2, 3, 0,
     };
+
+    unsigned int vao;
+    GLCall(glGenVertexArrays(1,&vao));
+    GLCall(glBindVertexArray(vao));
+
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions,GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions,GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2,GL_FLOAT,GL_FALSE, sizeof(float) * 2, 0);
@@ -165,10 +173,6 @@ int main(void) {
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices,GL_STATIC_DRAW);
 
     ShaderProgramSource source = ParseShader("../res/shaders/Basic.shader");
-    Log("Vertex");
-    Log(source.VertexSource);
-    Log("Fragment");
-    Log(source.FragmentSource);
 
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     glUseProgram(shader);
@@ -176,6 +180,11 @@ int main(void) {
     GLCall(int location = glGetUniformLocation(shader,"u_color"));
     ASSERT(location!=-1)
     GLCall(glUniform4f(location,0.2f,0.3f,0.8f,1.0f));
+
+    GLCall(glBindVertexArray(0));
+    GLCall(glUseProgram(0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
 
     float r = 0.2f;
     float increment = 0.05f;
@@ -185,7 +194,12 @@ int main(void) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        GLCall(glUseProgram(shader));
         GLCall(glUniform4f(location,r,0.3f,0.8f,1.0f));
+
+        GLCall(glBindVertexArray(vao));
+        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+
         GLCall(glDrawElements(GL_TRIANGLES, 6,GL_UNSIGNED_INT, nullptr));
 
         if (r > 1.0f) {
