fe231b97ddc94a4ffe545bc3823e73145f6adcaa
auronvila <auronvila1121@gmail.com>
2025-04-20 14:47:22 +0300

    control rendered image position using imgui

--- src/Application.cpp
+++ src/Application.cpp
@@ -47,10 +47,10 @@ int main() {
     }
 
     float positions[] = {
-        300.0f, -200.0f, 0.0f, 0.0f, // 0 index
-        100.0f, -200.0f, 1.0f, 0.0f, // 1
-        100.0f, 200.0f, 1.0f, 1.0f, // 2
-        300.0f, 200.0f, 0.0f, 1.0f, // 3
+        100.0f, 100.0f, 0.0f, 0.0f, // 0 index
+        200.0f, 100.0f, 1.0f, 0.0f, // 1
+        200.0f, 200.0f, 1.0f, 1.0f, // 2
+        100.0f, 200.0f, 0.0f, 1.0f, // 3
     };
 
     // render rectangle based on indexes
@@ -77,15 +77,11 @@ int main() {
     IndexBuffer ib(indices, 6);
 
     glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-    glm::mat4 view = glm::translate(glm::mat4(1.0f),glm::vec3(-100,0,0));
-    glm::mat4 model = glm::translate(glm::mat4(1.0f),glm::vec3(200,200,0));
-
-    glm::mat4 mvp = proj * view * model;
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
 
     Shader shader("../res/shaders/Basic.shader");
     shader.Bind();
     shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
-    shader.SetUniformMat4f("u_MVP",mvp);
 
     Texture texture("../res/textures/webhook-png.png");
     texture.Bind();
@@ -103,12 +99,9 @@ int main() {
     ImGui_ImplOpenGL3_Init("#version 330");
     ImGui::StyleColorsDark();
 
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
     float r = 0.0f;
     float increment = 0.05f;
+    glm::vec3 translation(200, 200, 0);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
@@ -118,35 +111,28 @@ int main() {
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+        glm::mat4 mvp = proj * view * model;
+
         shader.Bind();
-        shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+        // shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+        shader.SetUniformMat4f("u_MVP", mvp);
 
         renderer.Draw(va, ib, shader);
 
-        if (r > 1.0f) {
-            increment = -0.05f;
-        } else if (r < 0.0f) {
-            increment = 0.05f;
-        }
+        // if (r > 1.0f) {
+        //     increment = -0.05f;
+        // } else if (r < 0.0f) {
+        //     increment = 0.05f;
+        // }
 
-        r += increment;
+        r += increment; {
+            ImGui::Text("Hello, world!"); // Display some text (you can use a format string too)
+            ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
+            // Edit 1 float using a slider from 0.0f to 1.0f
 
-        {
-            static float f = 0.0f;
-            static int counter = 0;
-            ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,
+                        ImGui::GetIO().Framerate);
         }
 
         ImGui::Render();
