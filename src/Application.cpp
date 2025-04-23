c192cbc1fef9862b8ad566ef26e4f3baf3f98ff6
auronvila <auronvila1121@gmail.com>
2025-04-23 20:05:52 +0300

    add second image with the same image

--- src/Application.cpp
+++ src/Application.cpp
@@ -47,10 +47,10 @@ int main() {
     }
 
     float positions[] = {
-        100.0f, 100.0f, 0.0f, 0.0f, // 0 index
-        200.0f, 100.0f, 1.0f, 0.0f, // 1
-        200.0f, 200.0f, 1.0f, 1.0f, // 2
-        100.0f, 200.0f, 0.0f, 1.0f, // 3
+        -50.0f, -50.0f, 0.0f, 0.0f, // 0 index
+        50.0f, -50.0f, 1.0f, 0.0f, // 1
+        50.0f, 50.0f, 1.0f, 1.0f, // 2
+        -50.0f, 50.0f, 0.0f, 1.0f, // 3
     };
 
     // render rectangle based on indexes
@@ -77,7 +77,7 @@ int main() {
     IndexBuffer ib(indices, 6);
 
     glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
     Shader shader("../res/shaders/Basic.shader");
     shader.Bind();
@@ -101,7 +101,8 @@ int main() {
 
     float r = 0.0f;
     float increment = 0.05f;
-    glm::vec3 translation(200, 200, 0);
+    glm::vec3 translationA(200, 200, 0);
+    glm::vec3 translationB(400, 200, 0);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
@@ -110,13 +111,20 @@ int main() {
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
+        shader.Bind(); {
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+            glm::mat4 mvp = proj * view * model;
+            // shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+            shader.SetUniformMat4f("u_MVP", mvp);
+            renderer.Draw(va, ib, shader);
+        } {
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+            glm::mat4 mvp = proj * view * model;
 
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-        glm::mat4 mvp = proj * view * model;
-
-        shader.Bind();
-        // shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-        shader.SetUniformMat4f("u_MVP", mvp);
+            // shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+            shader.SetUniformMat4f("u_MVP", mvp);
+            renderer.Draw(va, ib, shader);
+        }
 
         renderer.Draw(va, ib, shader);
 
@@ -126,9 +134,12 @@ int main() {
         //     increment = 0.05f;
         // }
 
-        r += increment; {
+        r += increment;
+        // * imgui window
+        {
             ImGui::Text("Hello, world!"); // Display some text (you can use a format string too)
-            ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
+            ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
+            ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
             // Edit 1 float using a slider from 0.0f to 1.0f
 
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,
